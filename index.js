7e1123cc6d8f5039d9f11fda140e7ac3ec5f2334
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-10 10:40:23 -0700

    added saturdayFun function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(fun){
+  if (fun){
+    return `This Saturday, I want to ${fun}!`;
+  } else {
+    return 'This Saturday, I want to roller-skate!';
+  }
+}
+console.log(saturdayFun('keep coding'))
+
+
